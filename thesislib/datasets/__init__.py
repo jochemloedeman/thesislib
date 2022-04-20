de7f284e807451989e7eedc254b00c46ffcd8003
Jochem Loedeman <j.m.loedeman@gmail.com>
2022-04-20 14:56:17 +0200

    Add package imports

--- thesislib/datasets/__init__.py
+++ thesislib/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .coco_eval import COCOKarpathy
-from .coco_trainval import COCOCaptions
+from .coco_trainval import COCOCaptions, caption_collate
 from .flickr30k import Flickr30kDataset
 from .msrvtt import MSRVTTFrames
