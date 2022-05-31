d7937248478dbc3dbfe981b49440c07194b54544
Jochem Loedeman <j.m.loedeman@gmail.com>
2022-05-31 23:07:00 +0200

    Updated env for kinetics

--- thesislib/datasets/kinetics400.py
+++ thesislib/datasets/kinetics400.py
@@ -4,12 +4,22 @@ from torchvision import datasets
 
 if __name__ == '__main__':
     root_dir = '/project/prjsloedeman/data'
+    # root_dir = (Path(__file__).parents[3] / 'data').as_posix()
     kinetics400_train = datasets.Kinetics(
         root=root_dir,
         frames_per_clip=2,
         num_classes='400',
         num_workers=4,
         num_download_workers=4,
-        download=True,
+        # download=True,
         split='val'
+    )
+    kinetics400_val = datasets.Kinetics(
+        root=root_dir,
+        frames_per_clip=2,
+        num_classes='400',
+        num_workers=4,
+        num_download_workers=4,
+        # download=True,
+        split='train'
     )
