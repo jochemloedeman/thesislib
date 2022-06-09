f3d45358520e3ed37e7fb4660b1536314e3bc2f2
Jochem Loedeman <j.m.loedeman@gmail.com>
2022-06-09 13:32:33 +0200

    Remove test video list

--- thesislib/datamodules/ssv2_datamodule.py
+++ thesislib/datamodules/ssv2_datamodule.py
@@ -7,7 +7,7 @@ import torchvision.transforms
 from torch.utils.data import DataLoader
 from torchvision.transforms import InterpolationMode
 
-from thesislib.datasets import Kinetics, VideoFrameDataset
+from thesislib.datasets import VideoFrameDataset
 from thesislib.datasets.video_dataset import ImglistToTensor
 from thesislib.temporal import TemporalLabel
 
