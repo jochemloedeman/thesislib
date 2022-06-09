e67a92cb7032f2f151fa970142e3fc120f5d750e
Jochem Loedeman <j.m.loedeman@gmail.com>
2022-06-09 19:59:42 +0200

    Remove test video list

--- thesislib/models/temporal_clip.py
+++ thesislib/models/temporal_clip.py
@@ -99,9 +99,10 @@ class TemporalCLIP(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         frames, labels = batch
         logits_per_video, logits_per_text = self(frames)
+        preds_best = logits_per_video.argmax(dim=-1)
         self.top1_accuracy(logits_per_video, labels)
         self.top5_accuracy(logits_per_video, labels)
-        self.classwise_accuracy(logits_per_video, labels)
+        self.classwise_accuracy(preds_best, labels)
 
     def test_epoch_end(self, outputs) -> None:
         acc_per_class = self.classwise_accuracy.compute()
